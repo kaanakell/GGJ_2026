19163263bf6147e286ffec99b0680b6bafd1b6d9
Cagdas <cagkar07@gmail.com>
2026-02-01 00:59:19 +0200

    The_best_ever_upload_in_the_world
    2 Mask and 3 Character assets taken from Paria. Their size should be adjusted.
    
    Soundtracks and text font called "Hunter" taken from Patrick and being used in UI in the "GameMenu" scene.
    
    Cagdas wrote simple code with AI to make credits part, added colors and clickable names in it (gives our websites/itch io/linkedin pages), and added temporarily background image called "3c2b2512-5d61-4469-9c45-0a59d7dc2e66" until someone draws better one.

---
+++ Assets/Scripts/Credits/CagdasKaratas.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class CagdasKaratas : MonoBehaviour,
+    IPointerClickHandler,
+    IPointerEnterHandler,
+    IPointerExitHandler  
+{
+    [Header("Itch.io URL")]
+    [SerializeField] private string itchIoUrl = "https://cagkar07.itch.io/";
+
+    [Header("Text Colors")]
+    [SerializeField] private Color normalColor = Color.white;
+    [SerializeField] private Color hoverColor = Color.yellow;
+
+    private TextMeshProUGUI tmpText;
+
+    void Awake()
+    {
+        tmpText = GetComponent<TextMeshProUGUI>();
+
+        if (tmpText != null)
+        {
+            normalColor = tmpText.color;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Application.OpenURL(itchIoUrl);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (tmpText != null)
+        {
+            tmpText.color = hoverColor;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (tmpText != null)
+        {
+            tmpText.color = normalColor;
+        }
+    }
+}
---
+++ Assets/Scripts/Credits/PariaAkhavass.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class PariaAkhavass : MonoBehaviour,
+    IPointerClickHandler,
+    IPointerEnterHandler,
+    IPointerExitHandler
+{
+    [Header("Website URL")]
+    [SerializeField] private string websiteUrl = "https://www.pariaakhavass.com/";
+
+    [Header("Text Colors")]
+    [SerializeField] private Color normalColor = Color.white;
+    [SerializeField] private Color hoverColor = Color.cyan;
+
+    private TextMeshProUGUI tmpText;
+
+    void Awake()
+    {
+        tmpText = GetComponent<TextMeshProUGUI>();
+        if (tmpText != null)
+        {
+            normalColor = tmpText.color;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Application.OpenURL(websiteUrl);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (tmpText != null)
+        {
+            tmpText.color = hoverColor;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (tmpText != null)
+        {
+            tmpText.color = normalColor;
+        }
+    }
+}
